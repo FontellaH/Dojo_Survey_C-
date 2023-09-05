f671542bfd557c776ce463a4075ac3d2be0ac747
Fontella Hooper <hooperfontella6@gmail.com>
2023-09-05 10:48:36 -0700

    first commit

---
+++ DojoSurvey/Controllers/DojoController.cs
@@ -0,0 +1,52 @@
+
+using Microsoft.AspNetCore.Mvc;// #10 importing route
+
+
+
+namespace NewWeb.Controllers;  //#11  keeps the controllers talking to each other
+
+public class DojoController : Controller
+
+{
+
+    [HttpGet]  /// #13 showing im about to write a action
+    [Route("")]   // #14 Has a empty base route
+    public ViewResult DojoForm()  // #15writing the function here for the base route
+    {
+        return View("DojoForm");
+    }
+
+
+    [HttpGet("results")]
+    public ViewResult DojoResult()
+    {
+        return View();  //rendering the page
+    }
+
+
+    [HttpPost("process")]   // This makes the post for the form
+    public ViewResult Process(string name, string location, string language, string textarea)
+    {
+        ViewBag.name = name;
+        ViewBag.location = location;
+        ViewBag.language = language;
+        ViewBag.textarea = textarea;
+
+        
+        return View("DojoResult");
+    }
+
+
+     [HttpPost("processAnother")] // Add a different route for the second Process action
+        public ViewResult ProcessAnother(string name, string location, string language, string textarea)
+        {
+            ViewBag.name = name;
+            ViewBag.location = location;
+            ViewBag.language = language;
+            ViewBag.textarea = textarea;
+
+            return View("DojoResult");
+        }
+
+}
+
